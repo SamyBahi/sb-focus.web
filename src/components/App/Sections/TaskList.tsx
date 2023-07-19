bd06c896c675a610c73cffa4165bb10a65cd0dc7
Samy Bahi <samy@samybahi.com>
2023-07-19 18:29:46 +0200

    deleted useless file, improved dragging on phone

--- src/components/App/Sections/TaskList.tsx
+++ src/components/App/Sections/TaskList.tsx
@@ -45,15 +45,13 @@ const TaskList = (props: any) => {
   const taskIds = tasks.map((task: any) => task._id);
 
   const mouseSensor = useSensor(MouseSensor, {
-    // Require the mouse to move by 10 pixels before activating
     activationConstraint: {
       distance: 10,
     },
   });
   const touchSensor = useSensor(TouchSensor, {
-    // Press delay of 250ms, with tolerance of 5px of movement
     activationConstraint: {
-      delay: 250,
+      delay: 50,
       tolerance: 5,
     },
   });
